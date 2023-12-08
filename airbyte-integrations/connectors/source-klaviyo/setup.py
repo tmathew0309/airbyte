90177dbb07b3dab4dc992c693744ed7773c60951
brianjlai <brian.lai@airbyte.io>
2023-12-08 15:15:54 -0800

    add catalog with bad events and explicit cdk version

--- airbyte-integrations/connectors/source-klaviyo/setup.py
+++ airbyte-integrations/connectors/source-klaviyo/setup.py
@@ -5,7 +5,7 @@
 
 from setuptools import find_packages, setup
 
-MAIN_REQUIREMENTS = ["airbyte-cdk"]
+MAIN_REQUIREMENTS = ["airbyte-cdk~=0.57.0"]
 
 TEST_REQUIREMENTS = ["requests-mock~=1.9.3", "pytest~=6.1", "pytest-mock", "requests_mock~=1.8"]
 
